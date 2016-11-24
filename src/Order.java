0e53d0595502848b45334a20234df5e79f546f2e
cjlynch278 <cjlynch@smcm.edu>
2016-11-24 18:33:21 -0500

    Update Order.java

--- src/Order.java
+++ src/Order.java
@@ -33,25 +33,28 @@ import java.util.ArrayList;
 
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
+
 import java.awt.BorderLayout;
+
 import javax.swing.UIManager;
+
+import java.awt.Font;
 
 
 public class Order extends JPanel {
 	public int count = 1;
-	public ArrayList<String> orderList;
+	//public ArrayList<String> orderList;
 	/**
 	 * Create the panel.
+	 * @param orderList 
 	 */
-	public Order(JFrame frame, Launch launch , ArrayList<String> o, DefaultListModel<String> model) {
-		orderList = o;
+
+	public Order(Launch launch, DefaultListModel<String> model, ArrayList<String> orderList) {
 		setBackground(Color.LIGHT_GRAY);
-		for(int i =0; i<orderList.size() ; i++){
-			model.addElement((i+1) + ". " +orderList.get(i));
-		}
+		
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{200, 200, 0};
-		gridBagLayout.rowHeights = new int[]{100, 400, 0};
+		gridBagLayout.rowHeights = new int[]{50, 450, 0};
 		gridBagLayout.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
@@ -59,9 +62,9 @@ public class Order extends JPanel {
 		JPanel panel_3 = new JPanel();
 		panel_3.setBackground(Color.LIGHT_GRAY);
 		GridBagLayout gbl_panel_3 = new GridBagLayout();
-		gbl_panel_3.columnWidths = new int[]{100, 200, 75, 25, 0};
-		gbl_panel_3.rowHeights = new int[]{50, 50, 0};
-		gbl_panel_3.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_3.columnWidths = new int[]{143, 133, 250, 16, 0};
+		gbl_panel_3.rowHeights = new int[]{25, 25, 0};
+		gbl_panel_3.columnWeights = new double[]{1.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
 		gbl_panel_3.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
 		panel_3.setLayout(gbl_panel_3);
 		
@@ -75,7 +78,7 @@ public class Order extends JPanel {
 		panel_3.add(rigidArea_5, gbc_rigidArea_5);
 		
 		JLabel lblNewLabel_1 = new JLabel("Order");
-		lblNewLabel_1.setFont(lblNewLabel_1.getFont().deriveFont(lblNewLabel_1.getFont().getSize() + 20f));
+		lblNewLabel_1.setFont(lblNewLabel_1.getFont().deriveFont(lblNewLabel_1.getFont().getSize() + 60f));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.fill = GridBagConstraints.BOTH;
@@ -84,19 +87,6 @@ public class Order extends JPanel {
 		gbc_lblNewLabel_1.gridx = 1;
 		gbc_lblNewLabel_1.gridy = 0;
 		panel_3.add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
-		JButton btnNewButton_2 = new JButton("Check Out");
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		//btnNewButton_2.setIcon(new ImageIcon(Order.class.getResource("/Backicon/rsz_checkout-light.png")));
-		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
-		gbc_btnNewButton_2.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton_2.fill = GridBagConstraints.BOTH;
-		gbc_btnNewButton_2.gridx = 2;
-		gbc_btnNewButton_2.gridy = 1;
-		panel_3.add(btnNewButton_2, gbc_btnNewButton_2);
 		
 		Component rigidArea_7 = Box.createRigidArea(new Dimension(20, 20));
 		GridBagConstraints gbc_rigidArea_7 = new GridBagConstraints();
@@ -113,12 +103,29 @@ public class Order extends JPanel {
 		gbc_3.gridy = 0;
 		add(panel_3, gbc_3);
 		
+		JButton btnCheckou = new JButton("Checkout");
+		btnCheckou.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				launch.getReceipt().setVisible(true);
+				setVisible(false);
+				
+			}
+		});
+		btnCheckou.setIcon(new ImageIcon(Order.class.getResource("/Backicon/rsz_checkout-light.png")));
+		btnCheckou.setFont(btnCheckou.getFont().deriveFont(btnCheckou.getFont().getSize() + 10f));
+		GridBagConstraints gbc_btnCheckou = new GridBagConstraints();
+		gbc_btnCheckou.fill = GridBagConstraints.BOTH;
+		gbc_btnCheckou.insets = new Insets(0, 0, 0, 5);
+		gbc_btnCheckou.gridx = 2;
+		gbc_btnCheckou.gridy = 1;
+		panel_3.add(btnCheckou, gbc_btnCheckou);
+		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(Color.LIGHT_GRAY);
 		
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
 		gbl_panel_1.columnWidths = new int[]{25, 75, 75, 25, 0};
-		gbl_panel_1.rowHeights = new int[]{25, 100, 25, 100, 25, 50, 50, 0};
+		gbl_panel_1.rowHeights = new int[]{25, 100, 25, 100, 25, 60, 50, 0};
 		gbl_panel_1.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
 		gbl_panel_1.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
 		panel_1.setLayout(gbl_panel_1);
@@ -133,13 +140,10 @@ public class Order extends JPanel {
 		panel_1.add(rigidArea_4, gbc_rigidArea_4);
 		
 		JButton btnNewButton = new JButton("Pizza");
-		btnNewButton.setFont(btnNewButton.getFont().deriveFont(btnNewButton.getFont().getSize() + 9f));
+		btnNewButton.setFont(btnNewButton.getFont().deriveFont(btnNewButton.getFont().getSize() + 25f));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
 				launch.getPizza().setVisible(true);
-				launch.getPizza().repaint();
-				
 				setVisible(false);
 			}
 		});
@@ -153,6 +157,7 @@ public class Order extends JPanel {
 		
 		Component rigidArea_2 = Box.createRigidArea(new Dimension(20, 20));
 		GridBagConstraints gbc_rigidArea_2 = new GridBagConstraints();
+		gbc_rigidArea_2.fill = GridBagConstraints.BOTH;
 		gbc_rigidArea_2.gridwidth = 2;
 		gbc_rigidArea_2.insets = new Insets(0, 0, 5, 5);
 		gbc_rigidArea_2.gridx = 1;
@@ -176,7 +181,7 @@ public class Order extends JPanel {
 		panel_1.add(rigidArea, gbc_rigidArea);
 		
 		JButton btnNewButton_1 = new JButton("Beverage");
-		btnNewButton_1.setFont(btnNewButton_1.getFont().deriveFont(btnNewButton_1.getFont().getSize() + 9f));
+		btnNewButton_1.setFont(btnNewButton_1.getFont().deriveFont(btnNewButton_1.getFont().getSize() + 25f));
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				model.clear();
@@ -188,7 +193,7 @@ public class Order extends JPanel {
 					
 				}
 				
-				count++;
+				
 				
 			}
 		});
@@ -210,6 +215,7 @@ public class Order extends JPanel {
 		panel_1.add(rigidArea_3, gbc_rigidArea_3);
 		
 		JButton btnBack = new JButton("Back");
+		btnBack.setFont(btnBack.getFont().deriveFont(btnBack.getFont().getSize() + 10f));
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				launch.getMain().setVisible(true);
@@ -241,15 +247,16 @@ public class Order extends JPanel {
 		JPanel panel_2 = new JPanel();
 		panel_2.setBackground(Color.LIGHT_GRAY);
 		GridBagLayout gbl_panel_2 = new GridBagLayout();
-		gbl_panel_2.columnWidths = new int[]{175, 25};
-		gbl_panel_2.rowHeights = new int[]{325, 0, 50, 0};
-		gbl_panel_2.columnWeights = new double[]{1.0, 1.0};
-		gbl_panel_2.rowWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_2.columnWidths = new int[]{87, 87, 26};
+		gbl_panel_2.rowHeights = new int[]{350, 50, 25, 0};
+		gbl_panel_2.columnWeights = new double[]{1.0, 1.0, 1.0};
+		gbl_panel_2.rowWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
 		panel_2.setLayout(gbl_panel_2);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridwidth = 2;
 		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
 		gbc_scrollPane.fill = GridBagConstraints.BOTH;
 		gbc_scrollPane.gridx = 0;
@@ -263,7 +270,7 @@ public class Order extends JPanel {
 		
 		JLabel lblNewLabel = new JLabel("Cart");
 		scrollPane.setColumnHeaderView(lblNewLabel);
-		lblNewLabel.setFont(lblNewLabel.getFont().deriveFont(lblNewLabel.getFont().getSize() + 7f));
+		lblNewLabel.setFont(lblNewLabel.getFont().deriveFont(lblNewLabel.getFont().getSize() + 15f));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_1 = new GridBagConstraints();
 		gbc_1.fill = GridBagConstraints.BOTH;
@@ -286,18 +293,43 @@ public class Order extends JPanel {
 		
 		Component rigidArea_8 = Box.createRigidArea(new Dimension(20, 20));
 		GridBagConstraints gbc_rigidArea_8 = new GridBagConstraints();
-		gbc_rigidArea_8.fill = GridBagConstraints.VERTICAL;
+		gbc_rigidArea_8.fill = GridBagConstraints.BOTH;
 		gbc_rigidArea_8.insets = new Insets(0, 0, 5, 0);
-		gbc_rigidArea_8.gridx = 1;
+		gbc_rigidArea_8.gridx = 2;
 		gbc_rigidArea_8.gridy = 0;
 		panel_2.add(rigidArea_8, gbc_rigidArea_8);
-		btnDeleteSelection.setFont(btnDeleteSelection.getFont().deriveFont(btnDeleteSelection.getFont().getSize() + 4f));
+		btnDeleteSelection.setFont(btnDeleteSelection.getFont().deriveFont(btnDeleteSelection.getFont().getSize() + 10f));
 		GridBagConstraints gbc_btnDeleteSelection = new GridBagConstraints();
 		gbc_btnDeleteSelection.insets = new Insets(0, 0, 5, 5);
 		gbc_btnDeleteSelection.fill = GridBagConstraints.BOTH;
 		gbc_btnDeleteSelection.gridx = 0;
 		gbc_btnDeleteSelection.gridy = 1;
 		panel_2.add(btnDeleteSelection, gbc_btnDeleteSelection);
+		
+		JButton btnClearCart = new JButton("Clear Cart");
+		btnClearCart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				model.clear();
+				orderList.clear();
+				
+			}
+		});
+		btnClearCart.setFont(btnClearCart.getFont().deriveFont(btnClearCart.getFont().getSize() + 10f));
+		GridBagConstraints gbc_btnClearCart = new GridBagConstraints();
+		gbc_btnClearCart.fill = GridBagConstraints.BOTH;
+		gbc_btnClearCart.insets = new Insets(0, 0, 5, 5);
+		gbc_btnClearCart.gridx = 1;
+		gbc_btnClearCart.gridy = 1;
+		panel_2.add(btnClearCart, gbc_btnClearCart);
+		
+		Component rigidArea_9 = Box.createRigidArea(new Dimension(20, 20));
+		GridBagConstraints gbc_rigidArea_9 = new GridBagConstraints();
+		gbc_rigidArea_9.gridwidth = 2;
+		gbc_rigidArea_9.fill = GridBagConstraints.BOTH;
+		gbc_rigidArea_9.insets = new Insets(0, 0, 0, 5);
+		gbc_rigidArea_9.gridx = 0;
+		gbc_rigidArea_9.gridy = 2;
+		panel_2.add(rigidArea_9, gbc_rigidArea_9);
 
 	}
 }
