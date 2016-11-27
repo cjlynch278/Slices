56972b2b76f37c3e9932cadc352d7958c56dc1ce
SrJeffers <srjeffers@gmail.com>
2016-11-27 00:22:31 -0500

    Update EditAccounts.java

--- src/EditAccounts.java
+++ src/EditAccounts.java
@@ -17,6 +17,7 @@ import java.awt.Dimension;
 import javax.swing.DefaultListModel;
 import javax.swing.JTextField;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -35,7 +36,6 @@ import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.UIManager;
 
-
 public class EditAccounts extends JPanel {
 
 	/**
@@ -43,40 +43,39 @@ public class EditAccounts extends JPanel {
 	 */
 	public EditAccounts(JFrame frame, Launch launch, AccountDB accounts, DefaultListModel<String> AccountsModel) {
 		setBackground(Color.LIGHT_GRAY);
-		
+
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-		
+
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(Color.LIGHT_GRAY);
-		
+
 		JPanel panel_3 = new JPanel();
 		panel_3.setBackground(Color.LIGHT_GRAY);
 		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap())
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addContainerGap())
 				.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE)
-				.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 450, Short.MAX_VALUE)
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 450, Short.MAX_VALUE));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 221, Short.MAX_VALUE))
-		);
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE))
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 221, Short.MAX_VALUE)));
 		GridBagLayout gbl_panel_3 = new GridBagLayout();
-		gbl_panel_3.columnWidths = new int[]{0, 0};
-		gbl_panel_3.rowHeights = new int[]{52, 0};
-		gbl_panel_3.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel_3.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel_3.columnWidths = new int[] { 0, 0 };
+		gbl_panel_3.rowHeights = new int[] { 52, 0 };
+		gbl_panel_3.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panel_3.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
 		panel_3.setLayout(gbl_panel_3);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Modify Accounts");
 		lblNewLabel_1.setFont(lblNewLabel_1.getFont().deriveFont(lblNewLabel_1.getFont().getSize() + 60f));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
@@ -85,20 +84,14 @@ public class EditAccounts extends JPanel {
 		gbc_lblNewLabel_1.gridx = 0;
 		gbc_lblNewLabel_1.gridy = 0;
 		panel_3.add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
-		
 
-		
-		
-		
-		
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{40, 70, 70, 70, 40, 0};
-		gbl_panel_1.rowHeights = new int[]{20, 50, 13, 50, 20, 50, 10, 40, 10, 45, 20};
-		gbl_panel_1.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+		gbl_panel_1.columnWidths = new int[] { 40, 70, 70, 70, 40, 0 };
+		gbl_panel_1.rowHeights = new int[] { 20, 50, 13, 50, 20, 50, 10, 40, 10, 45, 20 };
+		gbl_panel_1.columnWeights = new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE };
+		gbl_panel_1.rowWeights = new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
 		panel_1.setLayout(gbl_panel_1);
-		
+
 		Component rigidArea_4 = Box.createRigidArea(new Dimension(20, 20));
 		GridBagConstraints gbc_rigidArea_4 = new GridBagConstraints();
 		gbc_rigidArea_4.fill = GridBagConstraints.HORIZONTAL;
@@ -107,7 +100,7 @@ public class EditAccounts extends JPanel {
 		gbc_rigidArea_4.gridx = 0;
 		gbc_rigidArea_4.gridy = 0;
 		panel_1.add(rigidArea_4, gbc_rigidArea_4);
-		
+
 		Component rigidArea = Box.createRigidArea(new Dimension(20, 20));
 		GridBagConstraints gbc_rigidArea = new GridBagConstraints();
 		gbc_rigidArea.fill = GridBagConstraints.BOTH;
@@ -116,7 +109,7 @@ public class EditAccounts extends JPanel {
 		gbc_rigidArea.gridx = 0;
 		gbc_rigidArea.gridy = 1;
 		panel_1.add(rigidArea, gbc_rigidArea);
-		
+
 		Component rigidArea_1 = Box.createRigidArea(new Dimension(20, 20));
 		GridBagConstraints gbc_rigidArea_1 = new GridBagConstraints();
 		gbc_rigidArea_1.insets = new Insets(0, 0, 5, 0);
@@ -125,7 +118,7 @@ public class EditAccounts extends JPanel {
 		gbc_rigidArea_1.gridx = 4;
 		gbc_rigidArea_1.gridy = 0;
 		panel_1.add(rigidArea_1, gbc_rigidArea_1);
-		
+
 		JButton btnBack = new JButton("Back");
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -133,7 +126,7 @@ public class EditAccounts extends JPanel {
 				setVisible(false);
 			}
 		});
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
 		gbc_scrollPane.gridheight = 5;
@@ -143,33 +136,37 @@ public class EditAccounts extends JPanel {
 		gbc_scrollPane.gridx = 1;
 		gbc_scrollPane.gridy = 1;
 		panel_1.add(scrollPane, gbc_scrollPane);
-		
-		
+
 		JLabel lblSelectAUser = new JLabel("Select A User To Modify");
 		scrollPane.setColumnHeaderView(lblSelectAUser);
 		lblSelectAUser.setFont(lblSelectAUser.getFont().deriveFont(lblSelectAUser.getFont().getSize() + 6f));
 		lblSelectAUser.setHorizontalAlignment(SwingConstants.CENTER);
-		
+
 		JList<String> list = new JList<String>(AccountsModel);
 		list.setFont(list.getFont().deriveFont(list.getFont().getSize() + 8f));
 		scrollPane.setViewportView(list);
-		
+
 		Component rigidArea_3 = Box.createRigidArea(new Dimension(20, 20));
 		GridBagConstraints gbc_rigidArea_3 = new GridBagConstraints();
 		gbc_rigidArea_3.insets = new Insets(0, 0, 5, 5);
 		gbc_rigidArea_3.gridx = 2;
 		gbc_rigidArea_3.gridy = 6;
 		panel_1.add(rigidArea_3, gbc_rigidArea_3);
-		
+
 		JButton btnDelete = new JButton("Edit");
 		btnDelete.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				accounts.setEdit(list.getSelectedIndex());
-				setVisible(false);				
-				
-				EditAccounts2 EditAccounts2 = new EditAccounts2(frame, launch, accounts, accounts.getEdit());
-				frame.getContentPane().add(EditAccounts2, "EditAccounts2");
-				EditAccounts2.setVisible(true);
+
+				if (list.getSelectedIndex() == -1) {
+					JOptionPane.showMessageDialog(null, "No entry was selected. Please select an index.");
+				} else {
+					accounts.setEdit(list.getSelectedIndex());
+					setVisible(false);
+
+					EditAccounts2 EditAccounts2 = new EditAccounts2(frame, launch, accounts, accounts.getEdit());
+					frame.getContentPane().add(EditAccounts2, "EditAccounts2");
+					EditAccounts2.setVisible(true);
+				}
 			}
 		});
 		btnDelete.setFont(btnDelete.getFont().deriveFont(btnDelete.getFont().getSize() + 8f));
@@ -179,7 +176,7 @@ public class EditAccounts extends JPanel {
 		gbc_btnDelete.gridx = 2;
 		gbc_btnDelete.gridy = 7;
 		panel_1.add(btnDelete, gbc_btnDelete);
-		
+
 		Component rigidArea_2 = Box.createRigidArea(new Dimension(20, 20));
 		GridBagConstraints gbc_rigidArea_2 = new GridBagConstraints();
 		gbc_rigidArea_2.insets = new Insets(0, 0, 5, 5);
@@ -193,7 +190,7 @@ public class EditAccounts extends JPanel {
 		gbc_btnBack.gridx = 1;
 		gbc_btnBack.gridy = 9;
 		panel_1.add(btnBack, gbc_btnBack);
-		
+
 		Component rigidArea_6 = Box.createRigidArea(new Dimension(20, 20));
 		GridBagConstraints gbc_rigidArea_6 = new GridBagConstraints();
 		gbc_rigidArea_6.fill = GridBagConstraints.BOTH;
